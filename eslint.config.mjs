11ca020d2a316fe0dfafca456ed4cd8087968d30
Acharya Group <acharyasumit786@gmail.com>
2025-08-25 14:12:44 +0530

    remove footer

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,46 +1,31 @@
-// import { dirname } from "path";
-// import { fileURLToPath } from "url";
-// import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname,
-// });
-
-// const eslintConfig = [
-//   ...compat.extends("next/core-web-vitals", "next/typescript"),
-//   {
-//     ignores: [
-//       "node_modules/**",
-//       ".next/**",
-//       "out/**",
-//       "build/**",
-//       "next-env.d.ts",
-//     ],
-//     rules: {
-//       'react/no-unescaped-entities': 'off',
-//       '@next/next/no-page-custom-font': 'off',
-//     },
-//   },
-// ];
-
-// export default eslintConfig;
-import { FlatCompat } from '@eslint/eslintrc'
- 
 const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-})
- 
+  baseDirectory: __dirname,
+});
+
 const eslintConfig = [
-  ...compat.config({
-    extends: ['next'],
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
+  {
     rules: {
-      'react/no-unescaped-entities': 'off',
-      '@next/next/no-page-custom-font': 'off',
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
     },
-  }),
-]
- 
-export default eslintConfig
+  },
+];
+
+export default eslintConfig;
